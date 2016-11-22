930cf0ea487c4cee5a8e91c6689ca39a69e38f15
olga250 <olga250595@rambler.ru>
2016-11-22 06:50:48 +0000

    urls

---
+++ ask/ask/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+urlpatterns = patterns('',  
+    url(r'^$', 'qa.views.test'),  
+    url(r'^login/$', 'qa.views.test'),  
+    url(r'^signup/$', 'qa.views.test'),  
+    url(r'^question/(\d+)$', 'qa.views.test'),  
+    url(r'^ask/.*$', 'qa.views.test'),  
+    url(r'^popular/$', 'qa.views.test'),  
+    url(r'^new/$', 'qa.views.test'),  
+    url(r'^question/(?P<q_id>\d+)/$','qa.views.draw_question', name = 'draw_question')
+)  
