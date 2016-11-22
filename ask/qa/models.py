54e2f4f2caa6737e1c1d3189ed9d2906e378fe75
olga250 <olga250595@rambler.ru>
2016-11-22 18:09:12 +0000

    model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,21 +1,25 @@
-from django.db import models
+from __future__ import unicode_literals 
+from django.db import models 
 from django.contrib.auth.models import User 
-# Create your models here.
+from django.core.urlresolvers import reverse 
+ # Create your models here. 
 class Question(models.Model): 
-     title = models.CharField(max_length=255) 
-     text = models.TextField() 
-     added_at = models.DateTimeField(blank=True, null=True) 
-     rating = models.IntegerField(default = 0) 
-     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) 
-     likes = models.ManyToManyField(User, related_name = '+') 
-     def __unicode__(self): 
-         return self.title 
+class Meta: 
+         db_table='question' 
+     title=models.CharField(max_length=255) 
+     text=models.TextField() 
+     added_at=models.DateTimeField(auto_now_add=True) 
+     rating=models.IntegerField(default = 0) 
+     author=models.ForeignKey(User,related_name='question_athor',blank=True,null=True) 
+     likes=models.ManyToManyField(User,related_name='question_likes',blank=True) 
+     def __str__(self): 
+     	return self.title 
 class Answer(models.Model): 
-     text = models.TextField() 
-     added_at = models.DateTimeField(blank=True, null=True) 
-     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL) 
-     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) 
-     def __unicode__(self): 
-         return self.text 
+class Meta: 
+         db_table='answer' 
+     text=models.TextField() 
+     added_at=models.DateTimeField(auto_now_add=True) 
+     question=models.ForeignKey(Question) 
+     author=models.ForeignKey(User) 
 
 
