9bdf76614dd33200da8cbe019074549580fa70b8
olga250 <olga250595@rambler.ru>
2016-11-22 18:46:50 +0000

    views

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -5,48 +5,58 @@
  from django.shortcuts import get_object_or_404 
  from models import Question, Answer 
  from django.core.paginator import Paginator, EmptyPage 
- def test(request, *args, **kwargs): 
-     return HttpResponse('OK', status=200) 
- def mainPage(request): 
-     try: 
-         page = int(request.GET.get('page', 1)) 
-     except ValueError: 
-         raise Http404 
-     limit = 10 
-     questions = Question.objects.order_by('-id') 
-     paginator = Paginator(questions, limit) 
-     paginator.baseurl = '/?page=' 
-     try: 
-         page = paginator.page(page) 
-     except EmptyPage: 
-         page = paginator.page(paginator.num_pages) 
-     return render(request, 'index.html', { 
-         'questions': page, 
-         'paginator': paginator, 'page': page, 
-     }) 
- def popular(request): 
-     try: 
-         page = int(request.GET.get('page', 1)) 
-     except ValueError: 
-         raise Http404 
-     limit = 10 
-     questions = Question.objects.order_by('-rating') 
-     paginator = Paginator(questions, limit) 
-     paginator.baseurl = '/popular/?page=' 
-     try: 
-         page = paginator.page(page) 
-     except EmptyPage: 
-         page = paginator.page(paginator.num_pages) 
-     return render(request, 'popular.html', { 
-         'questions': page, 
-         'paginator': paginator, 'page': page, 
-     }) 
- def question(request, slug): 
-     question = get_object_or_404(Question, id = slug) 
-     answers = Answer.objects.filter(question = question) 
-     return render(request, 'question.html', { 
-         'question': question, 
-         'answers' : answers, 
-     }) 
+ from django.core.urlresolvers import reverse 
+def test(request, *args, **kwargs): 
+     return HttpResponse('test') 
+ def index(request) : 
+ 	pageLimit = 10 
+ 	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc() 
+        qwests = Question.objects.all().order_by('-id') 
+ 	from django.core.paginator import Paginator 
+	page = request.GET.get('page') or 1 
+ 	try : 
+ 		page = int(page) 
+ 	except ValueError : 
+ 		page = 1 
+ 	paginator = Paginator(qwests, pageLimit) 
+ 	paginator.baseurl = '/?page=' 
+ 	try : 
+ 		page = paginator.page(page) 
+ 	except EmptyPage : 
+ 		page = paginator.page(paginator.num_pages) 
+ 	return render(request, 'questionList.html', { 
+ 		'title' : 'qwests and answers', 
+ 		'list' : page.object_list, 
+ 		'paginator' : paginator, 
+ 		'page' : page, 
+ 	}) 
+ def popular(request) : 
+ 	pageLimit = 10 
+ 	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc() 
+ 	qwests = Question.objects.all().order_by('-likes') 
+	from django.core.paginator import Paginator 
+	page = request.GET.get('page') or 1 
+ 	try : 
+ 		page = int(page) 
+ 	except ValueError : 
+ 		page = 1 
+ 	paginator = Paginator(qwests, pageLimit) 
+ 	paginator.baseurl = '/?page=' 
+ 	try : 
+ 		page = paginator.page(page) 
+ 	except EmptyPage : 
+ 		page = paginator.page(paginator.num_pages) 
+ 	return render(request, 'questionList.html', { 
+ 		'title' : 'popular quests', 
+ 		'list' : page.object_list, 
+ 		'paginator' : paginator, 
+ 		'page' : page, 
+ 	}) 
+ def question(request, id): 
+ question = get_object_or_404(Question, pk=id) 
+ answers = Answer.objects.filter(question = question) 
+ return render(request, 'question.html', { 'user':request.user, 'question':question, 'answers':answers, }) 
+
+ 
 
 
