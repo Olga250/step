cab0c50ce25d6b7aa96f897955b4c961db751c57
olga250 <olga250595@rambler.ru>
2016-11-22 18:40:24 +0000

    models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,25 +1,18 @@
-from __future__ import unicode_literals 
 from django.db import models 
 from django.contrib.auth.models import User 
-from django.core.urlresolvers import reverse 
  # Create your models here. 
 class Question(models.Model): 
-class Meta: 
-         db_table='question' 
-     title=models.CharField(max_length=255) 
-     text=models.TextField() 
-     added_at=models.DateTimeField(auto_now_add=True) 
-     rating=models.IntegerField(default = 0) 
-     author=models.ForeignKey(User,related_name='question_athor',blank=True,null=True) 
-     likes=models.ManyToManyField(User,related_name='question_likes',blank=True) 
-     def __str__(self): 
-     	return self.title 
+ 	title = models.CharField(max_length = 255) 
+ 	text = models.TextField() 
+ 	added_at = models.DateTimeField(auto_now_add = True) 
+ 	rating = models.IntegerField(default = 0) 
+ 	author = models.ForeignKey(User, related_name="question_author") 
+ 	likes = models.ManyToManyField(User, related_name="question_like", blank=True) 
+
 class Answer(models.Model): 
-class Meta: 
-         db_table='answer' 
-     text=models.TextField() 
-     added_at=models.DateTimeField(auto_now_add=True) 
-     question=models.ForeignKey(Question) 
-     author=models.ForeignKey(User) 
+ 	text = models.TextField() 
+ 	added_at = models.DateTimeField(auto_now_add = True) 
+ 	question = models.ForeignKey(Question) 
+ 	author = models.ForeignKey(User) 
 
 
